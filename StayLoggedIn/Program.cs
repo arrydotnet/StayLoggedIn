f64694f1c789ea3a5df1114f0ebec7b76b0a270e
arrydotnet <arry.net@gmail.com>
2020-12-14 15:23:13 +0530

    adding obj/bin

--- StayLoggedIn/Program.cs
+++ StayLoggedIn/Program.cs
@@ -19,8 +19,7 @@ namespace WindowsFormsApp1
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-          
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
         }
     }
 }
